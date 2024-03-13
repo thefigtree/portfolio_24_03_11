093d687c160606d36b73f8adce433b16d340af45
thefigtree <ldj8431@naver.com>
2024-03-14 00:01:57 +0900

    MainBanner Design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,40 +11,32 @@ export default function Home() {
   // console.log(x, y);
 
   return (
-    <main className={styles.main}>
-      <motion.div
-        className={styles.mask}
-        animate={{
-          WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
-          WebkitMaskSize: `${size}px`,
-        }}
-        transition={{ type: "tween", ease: "backOut" }}
-      >
-        <p
-          className={styles.text}
-          onMouseEnter={() => {
-            setIshover(true);
+    <>
+      <main className={styles.main}>
+        <motion.div
+          className={styles.mask}
+          animate={{
+            WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+            WebkitMaskSize: `${size}px`,
           }}
-          onMouseLeave={() => {
-            setIshover(false);
-          }}
+          transition={{ type: "tween", ease: "backOut" }}
         >
-          In computer science, syntactic sugar is syntax within a programming
-          language that is designed to make things easier to read or to express.
-          It makes the language "sweeter" for human use: things can be expressed
-          more clearly, more concisely, or in an alternative style that some may
-          prefer.
-        </p>
-      </motion.div>
-      <div className={styles.body}>
-        <p className={styles.text}>
-          사람이 이해 하고 표현하기 쉽게 디자인된 프로그래밍 언어 문법. 또는
-          프로그래밍 언어를 더욱 더 간결하고 명확하게 표현이 가능하도록. 즉,{" "}
-          <span className={styles.color}>Sweeter</span>하게 사용 할 수 있도록
-          도와주는 문법을{" "}
-          <span className={styles.color}>"Syntactic Sugar"</span>라고 한다.
-        </p>
-      </div>
-    </main>
+          <p
+            className={styles.text}
+            onMouseEnter={() => {
+              setIshover(true);
+            }}
+            onMouseLeave={() => {
+              setIshover(false);
+            }}
+          >
+            Syntactic Sugar.
+          </p>
+        </motion.div>
+        <div className={styles.body}>
+          <p className={styles.text}>Syntactic Sugar.</p>
+        </div>
+      </main>
+    </>
   );
 }
