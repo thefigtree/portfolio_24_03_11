63762ba041725eb8331e818380ee24b19d65db91
thefigtree <ldj8431@naver.com>
2024-04-16 22:45:56 +0900

    fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,26 @@
 "use client";
 import styles from "./page.module.css";
-import { motion } from "framer-motion";
-import { useState } from "react";
-import useMousePosition from "./../utils/useMousePosition";
+// import { motion } from "framer-motion";
+// import { useEffect, useState } from "react";
+// import useMousePosition from "./../utils/useMousePosition";
 
 export default function Home() {
-  const [isHover, setIshover] = useState(false);
-  const { x, y } = useMousePosition();
-  const size = isHover ? 400 : 25;
+  // useEffect(() => {
+  //   async () => {
+  //     const LocomotvieScroll = (await import("locomotive-scroll")).default;
+  //     const locomotiveScroll = new LocomotvieScroll();
+  //   };
+  // }, []);
+
+  // const [isHover, setIshover] = useState(false);
+  // const { x, y } = useMousePosition();
+  // const size = isHover ? 400 : 25;
   // console.log(x, y);
 
   return (
     <>
       <main className={styles.main}>
-        <motion.div
+        {/* <motion.div
           className={styles.mask}
           animate={{
             WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
@@ -35,7 +42,7 @@ export default function Home() {
         </motion.div>
         <div className={styles.body}>
           <p className={styles.text}>j !n</p>
-        </div>
+        </div> */}
       </main>
     </>
   );
