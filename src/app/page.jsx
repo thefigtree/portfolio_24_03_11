65b05faa229a164037f507d85ab2b0a0d8fcf199
thefigtree <ldj8431@naver.com>
2024-04-30 18:26:02 +0900

    End: Body

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -84,7 +84,52 @@ export default function Home() {
           </div>
 
           <div className={styles.right}>
-            <div className={styles.moContent}></div>
+            <div className={styles.moContent}>
+              <div className={styles.moRed}></div>
+              <h1>Red</h1>
+              <p>
+                Red is a color often associated with strong emotions such as
+                passion, love, and anger. It's a bold and attention-grabbing
+                color that can evoke feelings of excitement, warmth, and energy.
+              </p>
+              <div className={styles.moGreen}></div>
+              <h1>Green</h1>
+              <p>
+                Green is a color that is often associated with nature, growth,
+                and harmony. It is a calming and relaxing color that can evoke
+                feelings of balance, stability, and freshness. In color
+                psychology, green is said to represent balance and stability,
+                making it a popular choice for branding and marketing in the
+                health and wellness industry.
+              </p>
+              <div className={styles.moPink}></div>
+              <h1>Pink</h1>
+              <p>
+                Pink is a color that is often associated with femininity,
+                romance, and sweetness. It is a softer and more delicate shade
+                of red that can evoke feelings of warmth, love, and nurturing.
+                In the world of branding and marketing, pink is often used to
+                target a female audience or to promote products that are
+                associated with beauty, love, or romance.
+              </p>
+              <div className={styles.moBlue}></div>
+              <h1>Blue</h1>
+              <p>
+                Blue is a color that is often associated with calmness, trust,
+                and reliability. It is a peaceful and serene color that can
+                evoke feelings of stability, security, and professionalism. In
+                color psychology, blue is said to represent loyalty and trust,
+                making it a popular choice for branding and marketing in the
+                finance and technology industries.
+              </p>
+            </div>
+
+            <div className={styles.desktopGallery}>
+              <div className={styles.desktopRed}></div>
+              <div className={styles.desktopGreen}></div>
+              <div className={styles.desktopPink}></div>
+              <div className={styles.desktopBlue}></div>
+            </div>
           </div>
         </div>
         {/* <motion.div
