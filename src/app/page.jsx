401dfcba49c21f3e9fa26555cfa0eec6a5c41925
thefigtree <ldj8431@naver.com>
2024-08-08 23:35:31 +0900

    Issue: Scrolltrigger

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,27 +7,20 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 export default function Home() {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
-
     const details = gsap.utils.toArray(".section:not(:first-child)");
     const photos = gsap.utils.toArray(".desktopColor:not(:first-child)");
     gsap.set(photos, { yPercent: 101 });
-
     const allPhotos = gsap.utils.toArray(".desktopColor");
-
-    // requestAnimationFrame(animation);
-
+    // // requestAnimationFrame(animation);
     let mm = gsap.matchMedia();
-
     mm.add("(min-width: 600px)", () => {
       console.log("desktop");
-
       ScrollTrigger.create({
         trigger: ".gallery",
         start: "top top",
         end: "bottom bottom",
         pin: ".right",
       });
-
       details.forEach((detail, index) => {
         let headline = detail.querySelector("h1");
         let animation = gsap
@@ -46,7 +39,6 @@ export default function Home() {
         //   autoRefreshEvents: "visibilitychange,DOMContentLoaded,load",
         // });
       });
-
       return () => {
         // optional
         // custom cleanup code here (runs when it STOPS matching)
@@ -165,10 +157,10 @@ export default function Home() {
           </div>
 
           <div className="desktopGallery">
-            <div className="desktopColor"></div>
-            <div className="desktopColor"></div>
-            <div className="desktopColor"></div>
-            <div className="desktopColor"></div>
+            <div className="desktopColor">1</div>
+            <div className="desktopColor">2</div>
+            <div className="desktopColor">3</div>
+            <div className="desktopColor">4</div>
           </div>
         </div>
       </div>
