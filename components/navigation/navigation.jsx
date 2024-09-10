87ba64106c1ba49a52c511e00631c0b54422ed10
thefigtree <ldj8431@naver.com>
2024-09-10 23:02:48 +0900

    Css: Toggle Menu

--- components/navigation/navigation.jsx
+++ components/navigation/navigation.jsx
@@ -44,12 +44,35 @@ export default function Navigation() {
       </ul>
 
       <div className={style.toggle_menu} ref={container}>
-        <div className={style.menu_open} onClick={toggleMenu}>
-          <p>Menu</p>
+        <div className={style.toggle_menu_bar}>
+          <div className={style.menu_open} onClick={toggleMenu}>
+            <p>Menu</p>
+          </div>
         </div>
+
         <div className={style.menu_overlay}>
-          <div className={style.menu_close} onClick={toggleMenu}>
-            <p>Close</p>
+          <div className={style.menu_overlay_bar}>
+            <div className={style.menu_close} onClick={toggleMenu}>
+              <p>Close</p>
+            </div>
+          </div>
+          <div className={style.menu_close_icon}>
+            <p>&#x2715;</p>
+          </div>
+          <div className={style.menu_copy}>
+            <ul className={style.menu_links}>
+              {links.map((link) => {
+                return (
+                  <li className={style.menu_link_item} key={link.path}>
+                    <Link href={link.path}>{link.name}</Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+
+          <div className={style.menu_preview}>
+            <p>View Showreel</p>
           </div>
         </div>
       </div>
