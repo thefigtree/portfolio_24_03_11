861ac1aed4834eec2506ee662d3aa84806381777
thefigtree <ldj8431@naver.com>
2024-09-17 00:19:01 +0900

    func: Horizon Scroll

--- components/myStack/myStack.jsx
+++ components/myStack/myStack.jsx
@@ -1,15 +1,46 @@
 "use client";
 
+import { useRef } from "react";
 import "./myStack.css";
-// import { gsap } from "gsap";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
-// import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
 
 export default function MyStack() {
+  const sectionRef = useRef(null);
+  const triggerRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const pin = gsap.fromTo(
+      sectionRef.current,
+      {
+        translateX: 0,
+      },
+      {
+        translateX: "-300vw",
+        ease: "none",
+        duration: 1,
+        scrollTrigger: {
+          trigger: triggerRef.current,
+          start: "top top",
+          end: "2000 top",
+          scrub: 0.6,
+          pin: true,
+        },
+      }
+    );
+
+    return () => {
+      pin.kill();
+    };
+  });
+
   return (
     <section className="scroll_section_outer">
-      <div>
-        <div className="scroll_section_inner">
+      <div ref={triggerRef}>
+        <div className="scroll_section_inner" ref={sectionRef}>
           <div className="scroll_section">
             <h3>section 1</h3>
           </div>
